c4cd48839ff085af48b1e614b8cae477a83f21ac
anitha2962 <anithasivadas92@gmail.com>
2024-02-02 19:40:45 +0530

    'first-commit'

---
+++ abstraction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public abstract class Animal
+{
+    public abstract void makesound();
+}
+public class Dog : Animal
+{
+    public override void makesound()
+    {
+        Console.WriteLine("eeee");
+    }
+}
+class Program
+{
+    static void main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.makesound();
+    }
+}
